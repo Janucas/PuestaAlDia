3f1a2b5c42352e4ad33cb8a9704f36dde9100f68
Janucas <74763029+Janucas@users.noreply.github.com>
2022-04-08 09:35:46 +0200

    Subiendo maquina cafe

--- MaquinaCafe/src/Cafe.java
+++ MaquinaCafe/src/Cafe.java
@@ -1,31 +1,31 @@
 
 public class Cafe {
 
-	//Declaramos las variables
-	int cafe;
-	int leche;
-	int vasos;
-	double saldo;
-	final int TOTALCAFE=50;
-	final int TOTALECHE=50;
-	final int TOTALVASOS=80;
-	final int PRECIOCAFE=1;
-	final double PRECIOLECHE=0.8;
-	final double PRECIOCAFECONLECHE=1.5;
-	
-	//Declaramos el constructor principal
+	// Declaramos las variables
+	private int cafe;
+	private int leche;
+	private int vasos;
+	private double saldo;
+	private static final int TOTALCAFE = 50;
+	private static final int TOTALECHE = 50;
+	private static final int TOTALVASOS = 80;
+	private static final int PRECIOCAFE = 1;
+	private static final double PRECIOLECHE = 0.8;
+	private static final double PRECIOCAFECONLECHE = 1.5;
+
+	// Declaramos el constructor principal
 
 	public Cafe(double saldo) {
 		super();
-		if (this.saldo<0) {
+		if (this.saldo < 0) {
 			this.saldo = saldo;
 		}
-		this.cafe = this.TOTALCAFE;
-		this.leche = this.TOTALECHE;
-		this.vasos = this.TOTALVASOS;
+		this.cafe = Cafe.TOTALCAFE;
+		this.leche = Cafe.TOTALECHE;
+		this.vasos = Cafe.TOTALVASOS;
 	}
-	
-	//Declaramos los metodos setter y getter
+
+	// Declaramos los metodos setter y getter
 
 	public double getSaldo() {
 		return saldo;
@@ -46,76 +46,78 @@ public class Cafe {
 	public int getVasos() {
 		return vasos;
 	}
-	
-	//Declaramos el resto de metodos
-	
+
+	// Declaramos el resto de metodos
+
 	public void llenarVasos() {
-		this.cafe=this.TOTALCAFE;
-		this.leche=this.TOTALECHE;
-		this.vasos=this.TOTALVASOS;
+		this.cafe = this.TOTALCAFE;
+		this.leche = this.TOTALECHE;
+		this.vasos = this.TOTALVASOS;
 	}
-	
+
 	public String servirCafeSolo(double cantidad) {
 		String mensaje;
 		double vuelta;
-		if (cantidad<this.saldo-this.PRECIOCAFE) {
-			mensaje="No dispone de saldo suficiente";
-		}else if (this.vasos<=0 || this.cafe<=0){
-			mensaje="No quedan existencias";
-		}
-		else {
+		if (cantidad - this.PRECIOCAFE > this.saldo) {
+			mensaje = "No dispone de saldo suficiente";
+		} else if (this.vasos <= 0 || this.cafe <= 0) {
+			mensaje = "No quedan existencias";
+		} else if (cantidad < this.PRECIOCAFE) {
+			mensaje = "Introduzca la cantidad correcta que se le pide";
+		} else {
 			this.cafe--;
 			this.vasos--;
-			this.saldo+=this.PRECIOCAFE;
-			vuelta=cantidad-this.PRECIOCAFE;
-			mensaje="Aqui tiene su cafe, gracias por su visita.";
-			mensaje="Su vuelta es de: "+vuelta;
+			this.saldo += this.PRECIOCAFE;
+			vuelta = cantidad - this.PRECIOCAFE;
+			mensaje = "Aqui tiene su cafe, gracias por su visita.\n" + "Su vuelta es de: " + vuelta;
 		}
 		return mensaje;
 	}
-	
+
 	public String servirLecheSola(double cantidad) {
 		String mensaje;
 		double vuelta;
-		if (cantidad<this.saldo-this.PRECIOLECHE) {
-			mensaje="No dispone de saldo suficiente";
-		}else if (this.vasos<=0 || this.leche<=0){
-			mensaje="No quedan existencias";
-		}
-		else {
+		if (cantidad < this.saldo - this.PRECIOLECHE) {
+			mensaje = "No dispone de saldo suficiente";
+		} else if (this.vasos <= 0 || this.leche <= 0) {
+			mensaje = "No quedan existencias";
+		} else if (cantidad < this.PRECIOCAFE) {
+			mensaje = "Introduzca la cantidad correcta que se le pide";
+		} else {
 			this.leche--;
 			this.vasos--;
-			this.saldo+=this.PRECIOLECHE;
-			vuelta=cantidad-this.PRECIOLECHE;
-			mensaje="Aqui tiene su leche, gracias por su visita.";
-			mensaje="Su vuelta es de: "+vuelta;
+			this.saldo += this.PRECIOLECHE;
+			vuelta = cantidad - this.PRECIOLECHE;
+			mensaje = "Aqui tiene su leche, gracias por su visita.\n" + "Su vuelta es de: " + vuelta;
 		}
 		return mensaje;
 	}
-	
+
 	public String servirCafeConLeche(double cantidad) {
 		String mensaje;
 		double vuelta;
-		if (cantidad<this.saldo-this.PRECIOCAFECONLECHE) {
-			mensaje="No dispone de saldo suficiente";
-		}else if (this.vasos<=0 || this.leche<=0 || this.cafe<0){
-			mensaje="No quedan existencias";
-		}
-		else {
+		if (cantidad < this.saldo - this.PRECIOCAFECONLECHE) {
+			mensaje = "No dispone de saldo suficiente";
+		} else if (this.vasos <= 0 || this.leche <= 0 || this.cafe < 0) {
+			mensaje = "No quedan existencias";
+		} else if (cantidad < this.PRECIOCAFE) {
+			mensaje = "Introduzca la cantidad correcta que se le pide";
+		} else {
 			this.leche--;
 			this.cafe--;
 			this.vasos--;
-			this.saldo+=this.PRECIOCAFECONLECHE;
-			vuelta=cantidad-this.PRECIOCAFECONLECHE;
-			mensaje="Aqui tiene su leche, gracias por su visita.";
-			mensaje="Su vuelta es de: "+vuelta;
+			this.saldo += this.PRECIOCAFECONLECHE;
+			vuelta = cantidad - this.PRECIOCAFECONLECHE;
+			mensaje = "Aqui tiene su cafe con leche, gracias por su visita.\n" + "Su vuelta es de: " + vuelta;
 		}
 		return mensaje;
 	}
+
 	
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Cafe [cafe=" + cafe + ", leche=" + leche + ", vasos=" + vasos + ", saldo=" + saldo + "]";
+	}
+
 	
 }
---
+++ MaquinaCafe/src/Principal.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static Scanner teclado=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		//Declaramos el objeto
+		Cafe maquina1=new Cafe(20);
+
+		char opcion;
+		double cantidad=0;
+		
+		String menu=menu();
+		System.out.println("Bienvenido a tu maquina de cafe de confianza");
+		System.out.println(menu());
+		System.out.println("Elija una opcion");
+		opcion=teclado.nextLine().charAt(0);
+		
+		while (opcion != 5) {
+			switch (opcion) {
+			case '1':{
+				System.out.println("Introduce una cantidad");
+				cantidad=Double.parseDouble(teclado.nextLine());
+				System.out.println(maquina1.servirCafeSolo(cantidad));
+			}
+			break;
+			case '2':{
+				System.out.println("Introduce una cantidad");
+				cantidad=Double.parseDouble(teclado.nextLine());
+				System.out.println(maquina1.servirLecheSola(cantidad));
+			}
+			break;
+			case '3':{
+				System.out.println("Introduce una cantidad");
+				cantidad=Double.parseDouble(teclado.nextLine());
+				System.out.println(maquina1.servirCafeConLeche(cantidad));
+			}
+			break;
+			case '4':{
+				System.out.println(maquina1.toString());
+			}
+			break;
+			case '5':{
+				System.out.println("Apagar y salir");
+			}
+			break;
+			default:
+				System.out.println("Error");
+			}
+			System.out.println(menu());
+			System.out.println("Elija una opcion");
+			opcion=teclado.nextLine().charAt(0);
+			}
+		}
+
+	
+
+	private static String menu() {
+		return "1-.Servir cafe solo (1 euro)\n"+"2-.Servir leche (0.8 euros)\n"+"3-.Servir cafe con leche (1.5 euros)\n"+
+	"4-.Consultar estado de la maquina\n"+"5-.Apagar y salir";
+	}
+}
